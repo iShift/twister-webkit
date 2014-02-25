f6e5901faa296a0c48e7cfaf0354b42009d24518
Denis Ryabov <denis@mobilejoomla.com>
2014-02-25 19:14:48 +0400

    Separate "Quit" and "Quit & Shutdown" items

--- locale/ru-RU.js
+++ locale/ru-RU.js
@@ -3,4 +3,5 @@ translates = {
   'Minimize to tray': 'Сворачивать в трей',
   'Request attention': 'Уведомлять о сообщениях',
   'Quit': 'Выход',
+  'Quit & Shutdown': 'Остановка сервиса и выход',
 };
