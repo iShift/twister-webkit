d5c70af6e0cf5af20b70b06e9b99d6e187890cee
Denis Ryabov <denis@mobilejoomla.com>
2014-03-28 18:04:46 +0400

    update linux wrapper

--- build-linux/twister.sh
+++ build-linux/twister.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-export MYAPP_WRAPPER="`readlink -f "$0"`"
-HERE="`dirname "$MYAPP_WRAPPER"`"
-paths=(
-  "/lib/x86_64-linux-gnu/libudev.so.1" # Ubuntu, Xubuntu, Mint
-  "/usr/lib64/libudev.so.1" # SUSE, Fedora
-  "/usr/lib/libudev.so.1" # Arch, Fedora 32bit
-  "/lib/i386-linux-gnu/libudev.so.1" # Ubuntu 32bit
-)
-for i in "${paths[@]}"
-do
-  if [ -f $i ]
-  then
-    ln -s "$i" "$HERE/libudev.so.0"
-    break
-  fi
-done
+export TWISTER_WRAPPER="`readlink -f "$0"`"
+HERE="`dirname "$TWISTER_WRAPPER"`"
+
+if [ ! -f "$HERE/libudev.so.0" ]
+then
+  paths=(
+    "/lib/x86_64-linux-gnu/libudev.so.1" # Ubuntu, Xubuntu, Mint
+    "/usr/lib64/libudev.so.1" # SUSE, Fedora
+    "/usr/lib/libudev.so.1" # Arch, Fedora 32bit
+    "/lib/i386-linux-gnu/libudev.so.1" # Ubuntu 32bit
+  )
+  for i in "${paths[@]}"
+  do
+    if [ -f $i ]
+    then
+      ln -sn "$i" "$HERE/libudev.so.0"
+      break
+    fi
+  done
+fi
+
 if [[ -n "$LD_LIBRARY_PATH" ]]; then
   LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HERE"
 else
   LD_LIBRARY_PATH="$HERE"
 fi
 export LD_LIBRARY_PATH
-exec -a "$0" "$HERE/twister-bin" "$@"
+
+exec -a "$0" "$HERE/twister" "$@"
