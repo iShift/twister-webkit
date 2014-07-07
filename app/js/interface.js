4ed4637dd3aac351a4e616559d23c3d48b52c895
Denis Ryabov <denis@mobilejoomla.com>
2014-07-07 15:53:49 +0400

    additional check that window is not empty

--- app/js/interface.js
+++ app/js/interface.js
@@ -136,7 +136,7 @@
      */
     win.setWaitCursor = function (wait) {
         var doc = window.getIframeDocument();
-        if (doc) {
+        if (doc && doc.body) {
             doc.body.style.cursor = wait ? 'progress' : '';
         }
     };
