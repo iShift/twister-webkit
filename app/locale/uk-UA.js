94143337d394474bc1c60f45582e7fe1a6b55040
UA2004 <ua2004 at ukr.net>
2014-08-12 10:29:12 +0300

    Added Ukrainian localization

---
+++ app/locale/uk-UA.js
@@ -0,0 +1,19 @@
+translates = {
+    'Open Twister': 'Відкрити Twister',
+    'Themes': 'Тема оформлення',
+    'Minimize to tray': 'Згортати до панелі завдань',
+    'Request attention': 'Сповіщати про повідомлення',
+    'Always on Top': 'Поверх усіх вікон',
+    'Run Minimized': 'Запускати згорнутим',
+    'Restart': 'Перезапустити',
+    'Quit': 'Вийти',
+    'Open in browser': 'Відкрити у браузері',
+    'Copy': 'Скопіювати',
+    'Copy link': 'Скопіювати посилання',
+    'Paste': 'Вставити',
+    'Reload': 'Перезапустити',
+    'Cannot start twisterd.': 'Не вдалося запустити twisterd.',
+    'Restart again?': 'Перезапустити заново?',
+    'New version is available': 'Доступна нова версія',
+    'Download': 'Завантажити'
+};
